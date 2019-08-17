52a8b4c80a486ff4f1920a57af2c89fdaad1b1b3
Neftali Papelleras <iam@neftalipapelleras.com>
2019-08-18 00:01:26 +0800

    new: stepone react-material imple

--- src/App.js
+++ src/App.js
@@ -1,26 +1,190 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import 'typeface-roboto';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import StepOne from './StepOne';
+
+function Copyright() {
+    const classes = useStyles();
+
+    return (
+        <Typography
+            className={classes.footer}
+            variant="body2"
+            color="textSecondary"
+            align="center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            {'Copyright © '}
+            <Link color="inherit" href="https://neftalipapelleras.com/">
+                n3rfd
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'. Built with '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Material-UI.
+            </Link>
+        </Typography>
+    );
 }
 
-export default App;
+const useStyles = makeStyles(theme => ({
+    appBar: {
+        position: 'relative'
+    },
+    root: {
+        flexGrow: 1
+    },
+    menuButton: {
+        marginRight: theme.spacing(2)
+    },
+    title: {
+        flexGrow: 1
+    },
+    layout: {
+        width: 'auto',
+        marginLeft: theme.spacing(2),
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+            width: 600,
+            marginLeft: 'auto',
+            marginRight: 'auto'
+        }
+    },
+    paper: {
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+        padding: theme.spacing(2),
+        [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+            marginTop: theme.spacing(6),
+            marginBottom: theme.spacing(6),
+            padding: theme.spacing(3)
+        }
+    },
+    stepper: {
+        padding: theme.spacing(3, 0, 5)
+    },
+    buttons: {
+        display: 'flex',
+        justifyContent: 'flex-end'
+    },
+    button: {
+        margin: theme.spacing(1),
+        marginTop: theme.spacing(5)
+    },
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        textAlign: 'center',
+        marginBottom: theme.spacing(3)
+    }
+}));
+
+const steps = ['Step One', 'Step Two'];
+
+function getStepContent(step) {
+    switch (step) {
+        case 0:
+            return <StepOne />;
+        case 1:
+            return null;
+        case 2:
+            return null;
+        default:
+            throw new Error('Unknown step');
+    }
+}
+
+export default function App() {
+    const classes = useStyles();
+    const [activeStep, setActiveStep] = React.useState(0);
+
+    const handleNext = () => {
+        setActiveStep(activeStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep(activeStep - 1);
+    };
+
+    return (
+        <React.Fragment>
+            <CssBaseline />
+
+            <AppBar
+                position="static"
+                color="primary"
+                className={classes.appBar}
+            >
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Enrollment App
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            <main className={classes.layout}>
+                <Paper className={classes.paper}>
+                    <React.Fragment>
+                        {activeStep === steps.length ? (
+                            <React.Fragment>
+                                <Typography variant="h5" gutterBottom>
+                                    Thank you for your reservation.
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                    Your reservation number is #2001539. We have
+                                    emailed your reservation confirmation that
+                                    you can present to the office upon payment.
+                                </Typography>
+                            </React.Fragment>
+                        ) : (
+                            <React.Fragment>
+                                {getStepContent(activeStep)}
+                                <div className={classes.buttons}>
+                                    {activeStep !== 0 && (
+                                        <Button
+                                            onClick={handleBack}
+                                            className={classes.button}
+                                        >
+                                            Back
+                                        </Button>
+                                    )}
+                                    <Button
+                                        size="large"
+                                        variant="contained"
+                                        color="primary"
+                                        fullWidth={true}
+                                        onClick={handleNext}
+                                        className={classes.button}
+                                    >
+                                        {activeStep === steps.length - 1
+                                            ? 'Verify'
+                                            : 'Next'}
+                                    </Button>
+                                </div>
+                            </React.Fragment>
+                        )}
+                    </React.Fragment>
+                </Paper>
+            </main>
+            <Copyright />
+        </React.Fragment>
+    );
+}
---
+++ src/StepOne.js
@@ -0,0 +1,168 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const months = [
+    {
+        value: 1,
+        label: 'January'
+    },
+    {
+        value: 2,
+        label: 'Febuary'
+    },
+    {
+        value: 3,
+        label: 'March'
+    },
+    {
+        value: 4,
+        label: 'April'
+    },
+    {
+        value: 5,
+        label: 'May'
+    },
+    {
+        value: 6,
+        label: 'June'
+    },
+    {
+        value: 7,
+        label: 'July'
+    },
+    {
+        value: 8,
+        label: 'August'
+    },
+    {
+        value: 9,
+        label: 'September'
+    },
+    {
+        value: 10,
+        label: 'October'
+    },
+    {
+        value: 11,
+        label: 'November'
+    },
+    {
+        value: 12,
+        label: 'December'
+    }
+];
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap'
+    },
+    formControl: {
+        margin: theme.spacing(1)
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2)
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1)
+    },
+    typography: {
+        margin: theme.spacing(1),
+        textAlign: 'center'
+    }
+}));
+
+export default function AddressForm() {
+    const classes = useStyles();
+
+    const [birthday, setBirthday] = React.useState({
+        month: '-',
+        day: '',
+        year: ''
+    });
+
+    const handleChange = date => event => {
+        setBirthday({
+            ...birthday,
+            [date]: event.target.value
+        });
+    };
+
+    return (
+        <React.Fragment>
+            <Typography
+                variant="h6"
+                gutterBottom
+                className={classes.typography}
+            >
+                Enter your child's birthday
+            </Typography>
+            <Grid container>
+                <Grid item xs={5}>
+                    <FormControl fullWidth>
+                        <TextField
+                            id="outlined-select-month"
+                            select
+                            label="Month"
+                            className={classes.textField}
+                            value={birthday.month}
+                            onChange={handleChange('month')}
+                            SelectProps={{
+                                MenuProps: {
+                                    className: classes.menu
+                                }
+                            }}
+                            margin="normal"
+                            variant="outlined"
+                        >
+                            {months.map(option => (
+                                <MenuItem
+                                    key={option.value}
+                                    value={option.value}
+                                >
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={3}>
+                    <TextField
+                        id="outlined-number"
+                        label="Day"
+                        value={birthday.day}
+                        onChange={handleChange('day')}
+                        type="number"
+                        className={classes.textField}
+                        InputLabelProps={{
+                            shrink: true
+                        }}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    <TextField
+                        id="outlined-number"
+                        label="Year"
+                        value={birthday.year}
+                        onChange={handleChange('year')}
+                        type="number"
+                        className={classes.textField}
+                        InputLabelProps={{
+                            shrink: true
+                        }}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                </Grid>
+            </Grid>
+        </React.Fragment>
+    );
+}
